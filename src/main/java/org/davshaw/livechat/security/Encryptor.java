135b96cba9b36f6efa276f350f814680d33cf2a5
David Torres <juandis0246@gmail.com>
2024-02-12 15:12:40 -0500

    TODO: done

---
+++ src/main/java/org/davshaw/livechat/security/Encryptor.java
@@ -0,0 +1,23 @@
+package org.davshaw.livechat.security;
+
+import org.jasypt.util.text.BasicTextEncryptor;
+
+public class Encryptor {
+
+    private final String password = "M1HNk3fG7xGYJ0ZQ9zQ01xZHeNxGYJfNosvG8WKKuJkns7d1bAVsp7njfs";
+
+    BasicTextEncryptor textEncryptor;
+
+    public Encryptor() {
+        textEncryptor = new BasicTextEncryptor();
+        textEncryptor.setPassword(this.password);
+    }
+
+    public String encrypt(String input) {
+        return textEncryptor.encrypt(input);
+    }
+
+    public String decrypt(String input) {
+        return textEncryptor.decrypt(input);
+    }
+}
--- src/main/java/org/davshaw/livechat/service/DefaultAccountService.java
+++ src/main/java/org/davshaw/livechat/service/DefaultAccountService.java
@@ -2,126 +2,104 @@ package org.davshaw.livechat.service;
 
 import java.util.List;
 import java.util.Optional;
-
-import org.davshaw.livechat.repository.AccountRepository;
-import org.davshaw.livechat.security.Encrypter;
-import org.springframework.stereotype.Service;
-
+import lombok.RequiredArgsConstructor;
 import org.davshaw.livechat.entity.Account;
 import org.davshaw.livechat.exception.ConflictException;
 import org.davshaw.livechat.exception.NotFoundException;
-
-import lombok.RequiredArgsConstructor;
+import org.davshaw.livechat.repository.AccountRepository;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @SuppressWarnings("null")
 public class DefaultAccountService {
 
-    private static final Encrypter encrypter = new Encrypter(3);
+  private final AccountRepository accountRepository;
 
-    private final AccountRepository accountRepository;
-    /**
-     * Get all accounts from the database.
-     * @return
-     */
-    public List<Account> getAllAccounts() {
-        return accountRepository.findAll();
-    }
-    
-    /**
-     * Get an account by its ID.If the account doesn't exist, throw a NotFoundException.
-     * @param id
-     * @return
-     */
-    public Optional<Account> getAccountById(String id) {
-        if (!accountRepository.existsById(id)) {
-            String message = String.format(
-                "Account with ID %s doesn't exist.", id);
-            throw new NotFoundException(message);
-        }
+  /**
+   * Get all accounts from the database.
+   * @return
+   */
+  public List<Account> getAllAccounts() {
+    return accountRepository.findAll();
+  }
 
-        Optional<Account> account = accountRepository.findById(id);
-
-        Account accountValue = account.get();
-
-        // Decrypt the account's email, password, id and name
-        accountValue.setEmail(encrypter.decrypt(accountValue.getEmail()));
-        accountValue.setPassword(encrypter.decrypt(accountValue.getPassword()));
-        accountValue.setName(encrypter.decrypt(accountValue.getName()));
-        accountValue.setId(encrypter.decrypt(accountValue.getId()));
-
-        return Optional.of(accountValue);
+  /**
+   * Get an account by its ID.If the account doesn't exist, throw a NotFoundException.
+   * @param id
+   * @return
+   */
+  public Optional<Account> getAccountById(String id) {
+    if (!accountRepository.existsById(id)) {
+      String message = String.format("Account with ID %s doesn't exist.", id);
+      throw new NotFoundException(message);
     }
 
-    /**
-     * Create a new account. If the account already exists, throw a ConflictException.
-     * @param account
-     * @return
-     */
-    public Account createAccount(Account account) {
-        if (accountRepository.existsById(account.getId())) {
-            String message = String.format(
-                "Account with ID %s already exists.", account.getId());
-            throw new ConflictException(message);
-        }
+    Optional<Account> account = accountRepository.findById(id);
 
-        account.setPassword(encrypter.encrypt(account.getPassword()));
-        account.setEmail(encrypter.encrypt(account.getEmail()));
-        account.setId(encrypter.encrypt(account.getId()));
-        account.setName(encrypter.encrypt(account.getName()));
+    Account accountValue = account.get();
+    return Optional.of(accountValue);
+  }
 
-        return accountRepository.save(account);
+  /**
+   * Create a new account. If the account already exists, throw a ConflictException.
+   * @param account
+   * @return
+   */
+  public Account createAccount(Account account) {
+    if (accountRepository.existsById(account.getId())) {
+      String message = String.format(
+        "Account with ID %s already exists.",
+        account.getId()
+      );
+      throw new ConflictException(message);
     }
 
-    /**
-     * Update an account. If the account doesn't exist, throw a NotFoundException.
-     * @param account
-     * @return
-     */
-    public Account updateAccount(Account account) {
-        String encryptedId = encrypter.encrypt(account.getId());
-        if (!accountRepository.existsById(encryptedId)) {
-            String message = String.format(
-                "Account with ID %s doesn't exist.", account.getId());
-            throw new NotFoundException(message);
-        }
+    return accountRepository.save(account);
+  }
 
-        account.setPassword(encrypter.encrypt(account.getPassword()));
-        account.setEmail(encrypter.encrypt(account.getEmail()));
-        account.setId(encrypter.encrypt(account.getId()));
-        account.setName(encrypter.encrypt(account.getName()));
-        
-        return accountRepository.save(account);
+  /**
+   * Update an account. If the account doesn't exist, throw a NotFoundException.
+   * @param account
+   * @return
+   */
+  public Account updateAccount(Account account) {
+    if (!accountRepository.existsById(account.getId())) {
+      String message = String.format(
+        "Account with ID %s doesn't exist.",
+        account.getId()
+      );
+      throw new NotFoundException(message);
     }
 
-    /**
-     * Delete an account by its ID. If the account doesn't exist, throw a NotFoundException.
-     * @param id
-     */
-    public void deleteAccount(String id) {
-        if (!accountRepository.existsById(id)) {
-            String message = String.format(
-                "Account with ID %s doesn't exist.", id);
-            throw new NotFoundException(message);
-        }
-        accountRepository.deleteById(id);
-    }
+    return accountRepository.save(account);
+  }
 
-    /**
-     * Delete all accounts from the database.
-     */
-    public void deleteAllAccounts() {
-        accountRepository.deleteAll();
+  /**
+   * Delete an account by its ID. If the account doesn't exist, throw a NotFoundException.
+   * @param id
+   */
+  public void deleteAccount(String id) {
+    if (!accountRepository.existsById(id)) {
+      String message = String.format("Account with ID %s doesn't exist.", id);
+      throw new NotFoundException(message);
     }
+    accountRepository.deleteById(id);
+  }
 
-    /**
-     * Check if an account exists by its ID.
-     * @param id
-     * @return
-     */
-    public boolean existsAccountById(String id) {
-        return accountRepository.existsById(id);
-    }
-    
+  /**
+   * Delete all accounts from the database.
+   */
+  public void deleteAllAccounts() {
+    accountRepository.deleteAll();
+  }
+
+  /**
+   * Check if an account exists by its ID.
+   * @param id
+   * @return
+   */
+  public boolean existsAccountById(String id) {
+    return accountRepository.existsById(id);
+  }
 }
